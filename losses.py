400ddbee53f6292bf31ef80b1a3c21f96951c99e
周彬 <44090409+ZhoubinXM@users.noreply.github.com>
2019-10-30 21:04:23 +0800

    Add files via upload

--- losses.py
+++ losses.py
@@ -1,7 +1,7 @@
 import torch
 import random
 
-
+# 二进制交叉熵损失函数的稳定版本 用来实现数值的稳定 log+sum+exp
 def bce_loss(input, target):
     """
     Numerically stable version of the binary cross-entropy loss function.
@@ -16,23 +16,36 @@ def bce_loss(input, target):
     - A PyTorch Tensor containing the mean BCE loss over the minibatch of
       input data.
     """
+    """
+      二进制交叉熵损失函数的数值稳定版本。
+     根据https://github.com/pytorch/pytorch/issues/751
+     有关此公式的推导，请参见TensorFlow文档：
+     https://www.tensorflow.org/api_docs/python/tf/nn/sigmoid_cross_entropy_with_logits
+     输入：
+     -输入：形状（N，）的PyTorch张量给出分数。
+     -目标：形状（N，）的PyTorch张量包含0和1给出的目标。
+
+     输出：
+     -一个PyTorch张量，其中包含在
+       输入数据。
+    """
     neg_abs = -input.abs()
     loss = input.clamp(min=0) - input * target + (1 + neg_abs.exp()).log()
     return loss.mean()
 
-
+# 计算生成器的损失
 def gan_g_loss(scores_fake):
     """
     Input:
-    - scores_fake: Tensor of shape (N,) containing scores for fake samples
+    - scores_fake: Tensor of shape (N,) containing scores for fake samples 生成器生成样本的得分
 
     Output:
     - loss: Tensor of shape (,) giving GAN generator loss
     """
-    y_fake = torch.ones_like(scores_fake) * random.uniform(0.7, 1.2)
-    return bce_loss(scores_fake, y_fake)
+    y_fake = torch.ones_like(scores_fake) * random.uniform(0.7, 1.2)  #返回一个填充了标量值1的张量，其大小与之相同 input。
+    return bce_loss(scores_fake, y_fake)                              # random.uniform(x, y) 方法将随机生成一个实数，它在 [x,y] 范围内。
 
-
+# 计算判别器的损失
 def gan_d_loss(scores_real, scores_fake):
     """
     Input:
@@ -59,10 +72,18 @@ def l2_loss(pred_traj, pred_traj_gt, loss_mask, random=0, mode='average'):
     - mode: Can be one of sum, average, raw
     Output:
     - loss: l2 loss depending on mode
+    输入：
+     -pred_traj：形状的张量（seq_len，批处理，2）。 预测的轨迹。
+     -pred_traj_gt：形状的张量（seq_len，批处理，2）。 真实轨迹
+     预测。
+     -loss_mask：形状的张量（批处理，seq_len）
+     -模式：可以是总和，平均值，原始值之一
+     输出：
+     -损失：l2损失取决于模式
     """
     seq_len, batch, _ = pred_traj.size()
     loss = (loss_mask.unsqueeze(dim=2) *
-            (pred_traj_gt.permute(1, 0, 2) - pred_traj.permute(1, 0, 2))**2)
+            (pred_traj_gt.permute(1, 0, 2) - pred_traj.permute(1, 0, 2))**2)   # unsqueeze去掉纬度值为一的tensor permute将tensor的维度换位。
     if mode == 'sum':
         return torch.sum(loss)
     elif mode == 'average':
@@ -70,7 +91,7 @@ def l2_loss(pred_traj, pred_traj_gt, loss_mask, random=0, mode='average'):
     elif mode == 'raw':
         return loss.sum(dim=2).sum(dim=1)
 
-
+# 计算欧几里得误差
 def displacement_error(pred_traj, pred_traj_gt, consider_ped=None, mode='sum'):
     """
     Input:
@@ -94,10 +115,8 @@ def displacement_error(pred_traj, pred_traj_gt, consider_ped=None, mode='sum'):
     elif mode == 'raw':
         return loss
 
-
-def final_displacement_error(
-    pred_pos, pred_pos_gt, consider_ped=None, mode='sum'
-):
+#计算中带你的欧几里得误差
+def final_displacement_error(pred_pos, pred_pos_gt, consider_ped=None, mode='sum'):
     """
     Input:
     - pred_pos: Tensor of shape (batch, 2). Predicted last pos.
